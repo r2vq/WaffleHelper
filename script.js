cf37b93a61a223d26249f234326373de4c4e52ec
Keane Quibilan <arv.r2vq@gmail.com>
2022-06-29 11:25:40 -0400

    Add minimap

--- script.js
+++ script.js
@@ -39,12 +39,20 @@ function loadBoard(data) {
     let body = document.querySelector("body");
     let board = document.createElement("div");
     board.classList.add("board");
+    let minimap = document.createElement("div");
+    minimap.classList.add("minimap");
     let row;
+    let minirow;
     for (let i = 0; i < data.length; i++) {
       if (currentRow >= maxRow) {
         row = document.createElement("div");
         row.classList.add("row");
         board.appendChild(row);
+
+        minirow = document.createElement("div");
+        minirow.classList.add("row");
+        minimap.appendChild(minirow);
+
         currentRow = 0;
       }
       switch (i) {
@@ -57,6 +65,13 @@ function loadBoard(data) {
           space.classList.add("cell");
           space.classList.add("space");
           row.appendChild(space);
+
+          let minispace = document.createElement("div");
+          minispace.innerHTML = "&nbsp;";
+          minispace.classList.add("cell");
+          minispace.classList.add("space");
+          minirow.appendChild(minispace);
+
           currentRow += 1;
           break;
       }
@@ -65,18 +80,26 @@ function loadBoard(data) {
       cell.innerText = item.letter;
       cell.classList.add("cell");
       cell.classList.add("letter");
+      let minicell = document.createElement("div");
+      minicell.innerText = item.letter;
+      minicell.classList.add("cell");
+      minicell.classList.add("letter");
       if (item.green) {
         cell.classList.add("green");
+        minicell.classList.add("green");
       } else if (item.yellow) {
         cell.classList.add("yellow");
+        minicell.classList.add("yellow");
       }
       if (!item.green) {
         cell.addEventListener("click", onItemClick);
       }
       row.appendChild(cell);
+      minirow.appendChild(minicell);
       currentRow += 1;
     }
     body.innerHTML = "";
+    body.appendChild(minimap);
     body.appendChild(board);
   } else {
     body.innerHTML = "Something went wrong. Try refreshing.";
